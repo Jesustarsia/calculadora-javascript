4b19885ab95eeba250004c37132ea9d733284066
Jesustarsia <49409248+Jesustarsia@users.noreply.github.com>
2021-09-02 20:29:29 -0300

    Add files via upload

---
+++ funcionamiento.js
@@ -0,0 +1,45 @@
+var pantalla = document.getElementById('pantalla');
+
+
+
+function funcion1() {
+	pantalla.innerHTML=pantalla.innerHTML+"1";
+}
+
+function funcion2() {
+	pantalla.innerHTML=pantalla.innerHTML+"2";
+}
+
+function funcion3() {
+	pantalla.innerHTML=pantalla.innerHTML+"3";
+}
+function funcion4() {
+	pantalla.innerHTML=pantalla.innerHTML+"4";
+}
+
+function funcion5() {
+	pantalla.innerHTML=pantalla.innerHTML+"5";
+}
+
+function funcion6() {
+	pantalla.innerHTML=pantalla.innerHTML+"6";
+}
+
+function funcion7() {
+	pantalla.innerHTML=pantalla.innerHTML+"7";
+}
+
+function funcion8() {
+	pantalla.innerHTML=pantalla.innerHTML+"8";
+}
+
+function funcion9() {
+	pantalla.innerHTML=pantalla.innerHTML+"9";
+}
+
+function funcion0() {
+	pantalla.innerHTML=pantalla.innerHTML+"0";
+}
+function funcionpunto() {
+	pantalla.innerHTML=pantalla.innerHTML+".";
+}
